b000b873c99624edbd78789940f57537f269fa6e
Nash Baliga <bnbaliga@gmail.com>
2025-06-08 16:12:54 -0500

    Removed Unused Code
    Removed Unused Code

--- AzureSignalRClient/AzureSignalRClient/Models/NegotiationResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AzureSignalRClient.Models
-{
-    public class NegotiationResult
-    {
-        public string? Url { get; set; }
-        public string? Transport { get; set; } // Or other connection details
-    }
-}
